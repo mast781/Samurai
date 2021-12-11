b5d2345588460a9ab42f9d16aa7c840f1f86a032
Evgenyj <worker_lab@mail.ru>
2021-12-11 17:46:11 +0300

    Modify App.tsx add ul

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
-  return (
-    <div className="App">
-      Simple HTML
-    </div>
-  );
+    return (
+        <div>
+            <div>one div</div>
+            <div className="App">
+                <ul>
+                    <li>css</li>
+                    <li>html</li>
+                    <li>js</li>
+                    <li>react</li>
+                </ul>
+            </div>
+        </div>
+
+    );
 }
 
 export default App;
