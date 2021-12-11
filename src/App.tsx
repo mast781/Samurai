3ea574d888ca5b95a70c4d2a8ca8f806d04181b8
Evgenyj <worker_lab@mail.ru>
2021-12-11 18:26:09 +0300

    import Header and Technologies components from files

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,13 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./Header";
+import {Technologies} from "./Technologies";
 
 const App = () => {
     return (
         <div>
             <Header/>
             <Technologies/>
-        </div>
-    );
-}
-
-const Technologies = () => {
-    return (
-        <div>
-            <ul>
-                <li>css</li>
-                <li>html</li>
-                <li>js</li>
-                <li>react</li>
-            </ul>
-        </div>
-    );
-}
-
-const Header = () => {
-    return (
-        <div>
-            <a href="#">Home</a>
-            <a href="#">News Feed</a>
-            <a href="#">Messages</a>
         </div>
     );
 }
